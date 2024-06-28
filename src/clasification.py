2d4d5c7798dc2b4b22838c2f2e16bda67b741136
TAUscience <139826086+TAUscience@users.noreply.github.com>
2024-06-27 21:58:41 -0600

    V2 Clasificacion con PCA y grafica 3D

--- src/clasification.py
+++ src/clasification.py
@@ -1,13 +1,14 @@
-import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
+from mpl_toolkits.mplot3d import Axes3D
 
-def clasificar_etiquetar(ruta_ds,k,ruta_guardado):
-
+def clasificar_etiquetar(ruta_ds, k, ruta_guardado):
     """Carga y preparación de datos"""
     # Leer el archivo CSV llamado 'dataset.csv'
     data = pd.read_csv(ruta_ds)
@@ -21,6 +22,10 @@ def clasificar_etiquetar(ruta_ds,k,ruta_guardado):
     scaler = StandardScaler()
     X_imputed_scaled = scaler.fit_transform(imputer.fit_transform(X))
 
+    # Reducir las dimensiones a 3 utilizando PCA
+    pca = PCA(n_components=3)
+    X_pca = pca.fit_transform(X_imputed_scaled)
+
     # Inicializar KMeans
     kmeans = KMeans(n_clusters=k, random_state=42)
     kmeans.fit(X_imputed_scaled)
@@ -31,15 +36,24 @@ def clasificar_etiquetar(ruta_ds,k,ruta_guardado):
 
     # Agregar las etiquetas al DataFrame original
     data['cluster_label'] = kmeans.labels_
-    data.to_csv(ruta_guardado,index=False)
+    data.to_csv(ruta_guardado, index=False)
 
     # Mostrar un mensaje de confirmación
     print('Conjunto con etiquetas generado exitosamente.')
 
-    # Visualizar los clusters (opcional)
-    plt.scatter(X_imputed_scaled[:, 0], X_imputed_scaled[:, 1], c=kmeans.labels_, s=50, cmap='viridis')
-    centers = kmeans.cluster_centers_
-    plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75)
+    # Visualizar los clusters en 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=kmeans.labels_, s=50, cmap='viridis')
+    centers_pca = pca.transform(kmeans.cluster_centers_)
+    ax.scatter(centers_pca[:, 0], centers_pca[:, 1], centers_pca[:, 2], c='red', s=200, alpha=0.75)
+
+    # Leyenda y etiquetas
+    legend1 = ax.legend(*scatter.legend_elements(), title="Clusters")
+    ax.add_artist(legend1)
+    ax.set_xlabel('PCA Component 1')
+    ax.set_ylabel('PCA Component 2')
+    ax.set_zlabel('PCA Component 3')
     plt.show()
 
     return data
